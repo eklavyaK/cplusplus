ed658560c7a1e3aeab69e6bb81d27c839e3479f2
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-01-13 16:40:58 +0530

    commit

---
+++ Notes/Multimap.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main()
+{
+    multimap<int,string> map{{10,"messi"},{7,"cristiano"},{11,"neymar"},{8,"inesta"},{9,"suarez"},{10,"maradona"},{10,"pele"}};
+    multimap<int,int> m{{1,100},{2,200},{3,300},{4,400},{5,500}};
+    return 0;
+}
