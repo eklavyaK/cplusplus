9f92f8cd757d697315323b8b6331753b559784e7
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-01-23 20:40:02 +0530

    commit

--- testfile.cpp
+++ testfile.cpp
@@ -4,11 +4,9 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);cin.tie(NULL);
-    int t;cin>>t;
-    while(t-->0)
+    int tc;cin>>tc;
+    while(tc-->0)
     {
-        int n; cin>>n; int a[n]; vector<int> res;
-        for(int i = 0; i<n; i++) cin>>a[i];
         
     }
     return 0;
