7210d8875fac51a9c07b0082f0af5018594b035b
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-01-24 19:55:10 +0530

    Commit

--- testfile2.cpp
+++ testfile2.cpp
@@ -3,12 +3,9 @@ using namespace std;
 
 int main()
 {
-    int t; cin>>t;while(t--)
-    {
-        int n,x,k; cin>>n>>x;int o=0,e=0;
-        for(int i = 0; i<n; i++) {cin>>k;if(k%2)o++;else e++;}
-        if(o>0 && ((x%2 && o-!(o%2)+e>=x)||(x%2==0 && e!=0 && o-!(o%2)+e>=x))) cout<<"yes"<<endl;
-        else cout<<"no"<<endl;
-    }
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    cout<<"Hallelujah"<<endl;
+
     return 0;
 }
