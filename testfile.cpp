edde472f924b319ed1af49544b5f94b3e94fffc7
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-01-27 02:22:23 +0530

    commi

---
+++ testfile.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    
+    string s[3]; int in[3]; set<int> en; for(int i = 0; i<3; i++) {cin>>s[i]; en.insert(s[i][1]); in[i]=s[i][0];}
+    if(set.size())
+    return 0;
+}
