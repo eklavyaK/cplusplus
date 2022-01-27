455213ba9f18f8c7f6669f932aa8afb828489aed
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-01-28 03:39:01 +0530

    commit

---
+++ testfile.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int tc;cin>>tc;
+    while(tc-->0)
+    {
+        
+    }
+    return 0;
+}
