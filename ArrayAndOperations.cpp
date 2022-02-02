2276c31452583db4e844d5242c077b5d45ca7e8a
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-02-02 16:56:21 +0530

    commit

--- ArrayAndOperations.cpp
+++ ArrayAndOperations.cpp
@@ -9,7 +9,10 @@ int main()
     int tc;cin>>tc;
     while(tc-->0)
     {
-        
+        int n,k; cin>>n>>k; int a[n],sum=0;
+        for(int i = 0; i<n; i++) cin>>a[i];
+        sort(a,a+n); for(int i = n-1; i>=n-k; i--) {if(a[i-k]==a[i]) sum+=1;}
+        for(int i = 0; i<n-2*k; i++) sum+=a[i];cout<<sum<<endl;
     }
     return 0;
 }
