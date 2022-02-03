414504e3a2da1c1df09f2145fb455440047dc22f
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-02-03 12:33:13 +0530

    commit

--- testfile2.cpp
+++ testfile2.cpp
@@ -1,15 +1,11 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
-  ios_base::sync_with_stdio(false);cin.tie(NULL);
-  int tc;cin>>tc;
-  while(tc-->0)
-  {
-    int n,m; cin>>n>>m; int res[n*m];
-    for(int i = 0; i<n*m; i++){res[i]=max(i/m,n-1-i/m)+max(i%m,m-1-i%m);cout<<res[i]<<" ";}
-  }
-  return 0;
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+	
+
+	return 0;
 }
