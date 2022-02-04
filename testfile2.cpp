70bb78321b751d2dc169498554f06a46d0a0d044
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-02-04 20:13:45 +0530

    commit

--- testfile2.cpp
+++ testfile2.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);cin.tie(NULL);
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
 
-	
-
-	return 0;
+    int n,m; cin>>n; long long a[n],res, sum = 0l;
+    for(int i = 0; i<n; i++) {cin>>a[i];sum+=a[i];}
+    cin>>m;sort(a,a+n);while(m--) 
+    { 
+        long long x,y; res = upper_bound(a,a+n,x)-a;
+		cout<<res<<endl;
+	}
 }
