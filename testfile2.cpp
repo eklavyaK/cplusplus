c5fd6a28b0df0d64f2e23a838144362ee0dd82a8
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-02-10 04:29:54 +0530

    commit

--- testfile2.cpp
+++ testfile2.cpp
@@ -1,13 +1,11 @@
-#include<bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
+
 int main()
 {
-int t;
-cin>>t;while(t--)
-{
-string s, r; cin>>s>>r; int result = 0;
-for(int i=1; i<r.size();i++)
-if(r[i]!=r[i-1]) result+=abs((int)(s.find(r[i])-s.find(r[i-1])));
-cout<<result<<endl;
-}
+    int a[64] = {0};
+    for(int i = 1; i<=64; i++) cout<<i<<" ";cout<<endl;
+    for(int i = 64; i>=1; i--) cout<<i<<" "; 
+    return 0;
 }
