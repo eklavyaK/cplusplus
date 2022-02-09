6c9139ad2387421b3d7065394280d207821dc248
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-02-09 14:12:38 +0530

    commit

--- testfile.cpp
+++ testfile.cpp
@@ -9,14 +9,7 @@ int main()
     int tc;cin>>tc;
     while(tc-->0)
     {
-        int n; cin>>n; int a[n],o=0,e=0, count=0,track=1;
-        for(int i = 0; i<n; i++) {cin>>a[i];if(a[i]%2)o++;else e++;}
-        if(abs(o-e)>1) {cout<<-1<<endl;continue;}
-        if(o>e) {for(int i = 0; i<n; i++) if(a[i]%2==0){count=count+abs(track-i);track+=2;}}
-        else if(e>o) {for(int i = 0; i<n; i++) if(a[i]%2){count=count+abs(track-i);track+=2;}}
-        else {int r = 0;for(int i = 0; i<n; i++) if(a[i]%2==0){r=r+abs(track-i);track+=2;}int l=0;track=1;
-        for(int i = 0; i<n; i++) if(a[i]%2){l=l+abs(track-i);track+=2;} count = min(l,r);}
-        cout<<count<<endl;
+        
     }
     return 0;
 }
