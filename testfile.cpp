52e8922f3955aa2781ccc1c61babbe8de27ec48a
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-02-15 03:07:11 +0530

    commit

--- testfile.cpp
+++ testfile.cpp
@@ -1,15 +1,42 @@
 #include<bits/stdc++.h>
-#define removezeros(s) s.erase(0,s.find("1")!=-1?s.find("1"):s.length()-1)
-#define binary(n,k) bitset<k>(n).to_string()
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false);cin.tie(NULL);
-    int tc;cin>>tc;
-    while(tc-->0)
+    
+    int n,q; cin>>n>>q;
+    vector<vector<pair<int,int>>> val(n+1);
+    for(int i = 1; i<=n; i++) val[i].emplace_back(0,1);
+    vector<pair<int,int>> c(q+1);
+    for(int j = 1; j<=q; j++) 
     {
-        
+        string s; cin>>s;
+        if(s[0]=='Q') {
+            int k,l,r; cin>>k; long long sum=0; 
+            for(int m = 0; m<val[k].size()-1; m++) {
+                l = val[k][m].first+1; r=val[k][m+1].first;
+                int color = val[k][m].second;
+                for(int i = l; i<r; i++){
+                    if(c[i].first==color) sum+=c[i].second;
+                }
+            }
+            l = val[k][val[k].size()-1].first+1;
+            r = val[k][val[k].size()-1].second;
+            for(int i = l; i<j; i++){
+                if(c[i].first==r) sum+=c[i].second;
+            } 
+            cout<<sum<<endl;
+        }
+        else if(s[0]=='A'){
+            int t,x; cin>>t>>x;
+            c[j]=make_pair(t,x);
+        }
+        else {
+            int l,r,t; cin>>l>>r>>t;
+            for(int i = l; i<=r; i++) val[i].emplace_back(make_pair(j,t));
+        }
     }
+
     return 0;
 }
--- testfile2.cpp
+++ testfile2.cpp
@@ -3,11 +3,6 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);cin.tie(NULL);
-	int n; cin>>n; int max=0,index; vector<array<int,2>> v(2*n); 
-	for(int i = 0; i<2*n; i++) {cin>>v[i][0]; if(!(i%2))v[i][1]=1; else v[i][1]=0;} 
-	sort(v.begin(),v.end());int true_count=0; for(int i = 0; i<2*n; i++) 
-	for(int i = 0; i<2*n; i++) {if(v[i][1]) ++true_count; else --true_count;if(true_count>max) {max=true_count;index=i;}}
-	cout<<v[index][0]<<" "<<max<<endl;
-	return 0;
+	vector<vector<pair<int,int>>> v(5);
+	for(int i = 0; i<5; i++) cout<<v[i][0].first<<endl;
 }
