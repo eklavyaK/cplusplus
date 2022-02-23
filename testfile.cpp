b6b252464379be24a367f1d7bb18b2d68be12366
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-02-23 12:21:47 +0530

    commit

--- testfile.cpp
+++ testfile.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
+#define removezeros(s) s.erase(0,s.find("1")!=-1?s.find("1"):s.length()-1)
+#define binary(n,k) bitset<k>(n).to_string()
 using namespace std;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);cin.tie(NULL);
-	int tc; cin>>tc;
-	while(tc--){
-		int n,k; cin>>n>>k; int m=n;
-		if(k>=n){cout<<1<<endl;continue;}
-		for(int i = 2; i<=min(100000,k); i++)if(n%i==0){if(n/i<=k)m=min(m,i);else m=min(m,n/i);}
-		cout<<m<<endl;
+	int tc;cin>>tc;
+	while(tc-->0)
+	{
+		
 	}
 	return 0;
 }
