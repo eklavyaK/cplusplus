1078f9250728050dbe7657c600b8025e2acfeaf1
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-03-16 13:55:59 +0530

    commit

--- PermutationMinimizationByDeque.cpp
+++ PermutationMinimizationByDeque.cpp
@@ -11,7 +11,19 @@ int main()
     int tc;cin>>tc;
     while(tc-->0)
     {
-        
+        int n; cin>>n; int a[n];
+        for(int i = 0; i<n; i++)cin>>a[i];
+        deque<int> q;q.push_back(a[0]);
+        for(int i = 1; i<n; i++){
+            if(q.front()>a[i]){
+                q.push_front(a[i]);
+            }
+            else q.push_back(a[i]);
+        }
+        while(!q.empty()){
+            cout<<q.front()<<" ";q.pop_front();
+        }
+        cout<<endl;
     }
     return 0;
 }
