a13115dd450b806b067f1b24bc47eef723e619a9
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-03-17 15:52:25 +0530

    commit

--- Notes/SieveOfPrimes.cpp
+++ Notes/SieveOfPrimes.cpp
@@ -3,74 +3,51 @@ using namespace std;
 
 /*from 1 to 100000 (1 lakh) there are 9592 prime numbers, these primes numbers can divide each and every composite number from 1 to
 10000000000 (10^10)*/
-
-vector<int> v; // this the global array declared which will store all the primes from
+vector<int> v; // this is the global array declared which will store all the primes from t to u (both included)
 int counter;   // this is the actual number of primes stored in the array a after sieve is called using limits
-
-int sieve(int lim1, int lim2)
+int sieve(int t, int u)
 {
-	if (lim1 > lim2 || lim2 < 2)
-	{
-		cout << "Not a valid range!!" << endl;
+	if(t>u||u<2){
+		cout<<"Check the order of the range limits and make sure range contains at least one prime number."<<endl;
 		return 0;
 	}
-	else
-	{
-		if (lim1 <= 1)
-			lim1 = 2;
-		int range = lim2 - lim1 + 1;
-		vector<int> primes(range);
-		int multiplier = 0;
-		for (int count = 2; count < lim2; count++)
-		{
-			int start = pow(count, 2);
-			if (start > lim2)
-				break;
-			if (start < lim1)
-			{
-				if ((float)lim1 / count == lim1 / count)
-					multiplier = lim1 / count;
-				else
-					multiplier = lim1 / count + 1;
-				while (count * multiplier <= lim2)
-				{
-					if (primes[count * multiplier - lim1] != 1)
-						primes[count * multiplier - lim1] = 1;
-					multiplier++;
-				}
-			}
-			else
-			{
-				multiplier = count;
-				while (count * multiplier <= lim2)
-				{
-					if (primes[count * multiplier - lim1] != 1)
-						primes[count * multiplier - lim1] = 1;
-					multiplier++;
-				}
+	if(t<=1) t=2;
+	int range=(u-t+1);
+	vector<int> primes(range);
+	int c = 0;
+	for(int cnt=2; cnt<u; cnt++){
+		int start=pow(cnt, 2);
+		if(start>u) break;
+		if(start<t){
+			if (((float)t/cnt)==(t/cnt)) c=(t/cnt);
+			else c=(t/cnt)+1;
+			while(cnt*c<=u){
+				if(primes[cnt*c-t]!=1) primes[cnt*c-t]=1;
+				c++;
 			}
 		}
-		for (int i = lim1; i <= lim2; i++)
-			if (primes[i - lim1] != 1)
-			{
-				v.emplace_back(i);
-				counter++;
+		else{
+			c=cnt;
+			while(cnt*c<=u){
+				if(primes[cnt*c-t]!=1) primes[cnt*c-t]=1;
+				c++;
 			}
-		return 0;
+		}
 	}
+	for(int i=t; i<=u; i++){
+		if(primes[i-t]!=1){
+			v.emplace_back(i);
+			counter++;
+		}
+	}
+	return 0;
 }
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int start_limit, end_limit;
-	cin >> start_limit >> end_limit;
-	
-	sieve(start_limit, end_limit);
-
-	for (int i = 0; i < counter; i++)
-		cout << v[i] << " ";
-
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	int range_begin,range_end;
+	cin>>range_begin>>range_end;
+	sieve(range_begin,range_end);
+	for (int i=0; i<counter;i++)cout<<v[i]<<" ";
 	return 0;
 }
