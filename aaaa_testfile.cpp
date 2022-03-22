14095082c540cd6ec2a352180409e5dba60bdd88
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-03-22 13:23:49 +0530

    commit

--- aaaa_testfile.cpp
+++ aaaa_testfile.cpp
@@ -2,6 +2,10 @@
 #define removezeros(s) s.erase(0,s.find("1")!=-1?s.find("1"):s.length()-1)
 #define binary(n,k) bitset<k>(n).to_string()
 #define makeset(_set, _collection, size_of_collection) for(int i = 0; i<size_of_collection; i++){_set.emplace_back(_collection[i]);while(i+1<size_of_collection && _collection[i+1]==_collection[i])i++;}
+typedef long long ll;
+typedef long double ld;
+void Y(){std::cout<<"YES"<<std::endl;}
+void N(){std::cout<<"NO"<<std::endl;}
 using namespace std;
 
 int main(){
