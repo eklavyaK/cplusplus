2e1a226d0639d6704a80efe4c39eb7ce4db143d4
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-04-14 02:27:55 +0530

    commit

--- aaaa_testfile.cpp
+++ aaaa_testfile.cpp
@@ -9,13 +9,41 @@ typedef long double ld;
 void Y(){std::cout<<"YES"<<std::endl;}
 void N(){std::cout<<"NO"<<std::endl;}
 using namespace std;
-
 int main(){
     ios_base::sync_with_stdio(false);
     cout.precision(28);cin.tie(NULL);
     int tc;cin>>tc;
     while(tc-->0){
-        
+        ll sum = 0;
+        int n,k; cin>>n;int a[n];
+        for(int i = 0; i<n; i++){
+            cin>>a[i];
+            sum+=a[i];
+        }
+        vector<int> v(n,-1);
+        for(int i = 0; i<n-1; i++){
+            cin>>k;--k;v[k]++;
+            cin>>k;--k;v[k]++;
+        }
+        vector<pair<int,int>> c;
+        for(int i = 0; i<n; i++){
+            if(v[i]>0){
+                c.push_back(
+                    make_pair(
+                        a[i],v[i]
+                    )
+                );
+            }
+        }
+        sort(c.begin(),c.end());
+        cout<<sum<<' ';
+        int cnt = c.size()-1;
+        for(int i = 0; i<n-2; i++){
+            if(c[cnt].second==0)cnt--;
+            sum+=c[cnt].first;c[cnt].second--;
+            cout<<sum<<' ';
+        }
+        cout<<endl;
     }
     return 0;
 }
