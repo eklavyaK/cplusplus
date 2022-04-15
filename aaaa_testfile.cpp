d7d1d049660027c65e48acee734235fe1675800e
eklavyaK <eklavyakumarsingh100@gmail.com>
2022-04-15 13:15:55 +0530

    commit

--- aaaa_testfile.cpp
+++ aaaa_testfile.cpp
@@ -9,45 +9,16 @@ typedef long double ld;
 void Y(){std::cout<<"YES"<<std::endl;}
 void N(){std::cout<<"NO"<<std::endl;}
 using namespace std;
-
+int a[1000];
 int main(){
     ios_base::sync_with_stdio(false);
-    cout<<setprecision(6)<<fixed;cin.tie(NULL);
+    cout.precision(28);cin.tie(NULL);
+    for(int i = 0; i<1000; i++){
+        a[i]=i+1;
+    }
     int tc;cin>>tc;
     while(tc-->0){
-        int n,m,k=0; cin>>n>>m; int a[n];
-        for(int i = 0; i<n; i++)cin>>a[i];
-        vector<int> v(a,a+n);
-        sort(v.begin(),v.end());
-        for(int i=n-1;i>=0;i--){
-            if(v[i]!=a[i]){
-                k = i+1;break;
-            }
-        }
-        if(k==0){
-            ld f,c;
-            while(m--){
-                cin>>c>>f;
-            }
-            cout<<1<<endl;
-            continue;
-        }
-        else{
-            ld f,c;
-            ld result = 0;
-            while(m--){
-                cin>>c>>f;
-                if(c>=k){
-                    if(result==0){
-                        result=f;
-                    }
-                    else{
-                        result=result+f-result*f;
-                    }
-                }
-            }
-            cout<<min(result,(ld)1)<<endl;
-        }
+        int 
     }
     return 0;
 }
