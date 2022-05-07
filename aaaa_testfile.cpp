246ff59eb1f0ed3260111b7f1e0e0d4d30a76c92
eklavya <eklavyakumarsing100@gmail.com>
2022-05-07 15:55:37 +0530

    commit

--- aaaa_testfile.cpp
+++ aaaa_testfile.cpp
@@ -13,27 +13,9 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cout.precision(28);cin.tie(NULL);
-    ll x = 0; vector<int> a;int m = 1;
-    a.push_back(0);int k = 2,cnt=1;
-    while(x<1000000000){
-        if(k%3==0){
-            x+=4*cnt;
-            cnt++;
-            a.push_back(x);
-        }
-        else{
-            x+=2*m;
-            m++;
-            a.push_back(x);
-        }
-        k++;
-    }
-    cout<<a.size()<<endl;
     int tc;cin>>tc;
     while(tc-->0){
-        int n; cin>>n;int c = 0;
-        c = lower_bound(a.begin(),a.end(),n)-a.begin();
-        cout<<c+1<<endl;
+        
     }
     return 0;
 }
