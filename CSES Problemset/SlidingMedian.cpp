f86035533bf209fceddab5dffdbf476a8247159c
eklavya <eklavyakumarsingh100@gmail.com>
2022-05-31 18:52:00 +0530

    commit

--- CSES Problemset/SlidingMedian.cpp
+++ CSES Problemset/SlidingMedian.cpp
@@ -31,61 +31,23 @@ int main(){
         cout<<min(a[i].F,a[i+1].F)<<' ';exit(0);
     }
     multiset<pair<int,int>> st;
-    if(k%2){
-        for(int i=0;i<k;i++){
-            st.insert(a[i]);
-        }
-        auto it = st.begin();
-        advance(it,k/2);
-        cout<<it->F<<' ';
-        for(int i=k;i<n;i++){
-            st.insert(a[i]);
-            if(it==st.lower_bound(a[i-k])){
-                if(it->F<=a[i].F)it++;
-                else it--;
-            }
-            else if(it->F<a[i-k].F && it->F>a[i].F)it--;
-            else if(it->F<=a[i].F && it->F>=a[i-k].F)it++;
-            st.erase(a[i-k]);
-            cout<<it->F<<' ';
-        }
+    for(int i=0;i<k;i++){
+        st.insert(a[i]);
     }
-    else{
-        for(int i=0;i<k;i++){
-            st.insert(a[i]);
+    auto it = st.begin();
+    advance(it,k/2);
+    if(k%2==0)it--;
+    cout<<it->F<<' ';
+    for(int i=k;i<n;i++){
+        st.insert(a[i]);
+        if(it==st.lower_bound(a[i-k])){
+            if(it->F<=a[i].F)it++;
+            else it--;
         }
-        auto itf = st.begin();
-        auto ite = st.begin();
-        advance(itf,k/2-1);
-        advance(ite,k/2);
-        cout<<min(ite->F, itf->F)<<' ';
-        for(int i=k;i<n;i++){
-            st.insert(a[i]);
-            if(itf==st.lower_bound(a[i-k])){
-                if(a[i].F>=itf->F){
-                    if(a[i].F>=ite->F)ite++,itf++;
-                    else if(a[i].F<ite->F)itf++;
-                }
-                else itf--;
-            }
-            else if(ite==st.lower_bound(a[i-k])){
-                if(a[i].F>=itf->F){
-                    if(a[i].F<ite->F)ite--;
-                    else if(a[i].F>=ite->F)ite++;
-                }
-                else ite--,itf--;
-            }
-            else{
-                if(a[i].F<itf->F && a[i-k].F>ite->F)ite--,itf--;
-                else if(a[i-k].F<=itf->F && a[i].F>=ite->F)ite++,itf++;
-                else if(a[i].F<ite->F && a[i].F>=itf->F){
-                    if(a[i-k].F<=itf->F) itf++;
-                    else if(a[i-k].F>ite->F) ite--;
-                }
-            }
-            st.erase(a[i-k]);
-            cout<<min(ite->F,itf->F)<<' ';
-        }
+        else if(it->F<a[i-k].F && it->F>a[i].F)it--;
+        else if(it->F<=a[i].F && it->F>=a[i-k].F)it++;
+        st.erase(a[i-k]);
+        cout<<it->F<<' ';
     }
     return 0;
 }
