09adc318ac8c397b1bb846ce297bceab154cef3d
eklavya <eklavyakumarsingh100@gmail.com>
2023-01-15 21:10:11 +0530

    commit

---
+++ aaaa_testfile.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+typedef long double ld;
+typedef long long ll;
+using namespace std;
+
+
+void code(){
+    
+
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int t; cin>>t; while(t--)code();
+    return 0;
+}
