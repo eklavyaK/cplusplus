8aa1d7840923eeb53ffdc0a67de582e25d0850f5
eklavya <eklavyakumarsingh100@gmail.com>
2023-02-09 23:46:06 +0530

    commit

--- 1.cpp
+++ 1.cpp
@@ -10,12 +10,29 @@ using namespace std;
 
 
 void code(){
-    
-
+    int n,q; cin>>n>>q;
+    int arr[n], xr[n];
+    map<int,vector<int>> m;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        if(i==0)xr[i] = arr[i];
+        else xr[i] = xr[i-1]^arr[i];
+        m[xr[i]].push_back(i);
+    }
+    while(q--){
+        int l, r; cin>>l>>r;
+        l--,r--;
+        if((l==0 && xr[r]!=0) || (xr[l-1]!=xr[r])) cout<<-1<<endl;
+        else if((l-r+1)&1) cout<<1<<endl;
+        else if(!arr[l] || !arr[r]) cout<<1<<endl;
+        else{
+            
+        }
+    }
 }
 
 signed main(){
     cin.tie(0)->sync_with_stdio(0);
-    int t; cin>>t; while(t--)code();
+    int t=1; while(t--)code();
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,1 @@
-while(true){
-        if(st.count(end))break;
-        st.insert(end);
-        if(end+arr[end]<=n && end+arr[end]>=1 && next[end]>=0)end = next[end];
-        else  break;
-    }
+cout << ans << endl;
