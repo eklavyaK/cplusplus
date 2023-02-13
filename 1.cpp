792f3e30c6e87f3857c3adb8922620d7fef37ed0
eklavya <eklavyakumarsingh100@gmail.com>
2023-02-13 23:03:58 +0530

    commit

--- 1.cpp
+++ 1.cpp
@@ -10,8 +10,27 @@ using namespace std;
 
 
 void code(){
-    
-
+    int n,k; cin>>n>>k;
+    int done[n+1]{};
+    vector<int> ans(n);
+    if(k==1){
+        for(int i=1;i<=n;i++)cout<<i<<' ';
+        cout<<endl;
+        return;
+    }
+    int idx = 0;
+    int r = n, l = 1;
+    while(true){
+        for(int i=0;i<k-1;i++){
+            ans[idx++] = r--;
+            if(idx==n)goto end;
+        }
+        ans[idx++] = l++;
+        if(idx==n)goto end;
+    }
+    end:
+    for(auto i : ans) cout<<i<<' ';
+    cout<<endl;
 }
 
 signed main(){
