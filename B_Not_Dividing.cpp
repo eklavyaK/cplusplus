a1ab7b00fe0f59f99f410838530b83c17412863f
eklavya <eklavyakumarsingh100@gmail.com>
2023-03-05 16:32:10 +0530

    commit

--- B_Not_Dividing.cpp
+++ B_Not_Dividing.cpp
@@ -12,18 +12,16 @@ using namespace std;
 void code(){
     int n; cin>>n;
     vector<int> arr(n);
-    int cnt = 0;
-    for(int i=0;i<n;i++)cin>>arr[i];
     for(int i=0;i<n;i++){
-        int l = 0, r = i;
-        while(l<=r){
-            int mid = (l+r)>>1;
-            if((i+1-mid)<=arr[mid]) r = mid-1;
-            else l = mid+1;
-        }
-        cout<<i-r<<' ';
+        cin>>arr[i];
+        if(arr[i]==1)arr[i]++;
     }
-    cout<<endl;
+    for(int i=1;i<n;i++){
+        if(arr[i]%arr[i-1]==0){
+            arr[i]++;
+        }
+    }
+    for(auto i : arr)cout<<i<<' ';cout<<endl;
 }
 
 signed main(){
