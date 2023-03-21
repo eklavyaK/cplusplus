2ce030625af8e82d198a09b0597f032c4285a937
eklavya <eklavyakumarsingh100@gmail.com>
2023-03-21 18:43:49 +0530

    commit

--- 1.cpp
+++ 1.cpp
@@ -7,31 +7,27 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
-const int N = 1e6+5;
-bool f[N];
+const int mod = 998244353;
+
 void code(){
     int n; cin>>n;
+    vector<vector<int>> f(n+5,vector<int>(2));
     for(int i=0;i<n;i++){
         int k; cin>>k;
-        f[k] = 1;
-    }
-    for(int i=1e6;i>=1;i--){
-        if(f[i]) continue;
-        int g = 0;
-        for(int j=2;j*i<N;j++){
-            if(f[j*i])g = gcd(j*i,g);
-        }
-        if(g==i)f[i]=1;
+        int z = 0;
+        if(k)f[k-1][1]=(f[k-1][0]+2*f[k-1][1]+(k==1?1:0))%mod;;
+        f[k+1][0]=(f[k][0]+2*f[k+1][0]+(!k?1:0))%mod;
+        f[k+1][1]=2*f[k+1][1]%mod;
     }
     int ans = 0;
-    for(int i=1;i<N;i++){
-        if(f[i])ans++;
+    for(int i=0;i<=n;i++){
+        ans=(ans+f[i][0]+f[i][1])%mod;
     }
-    cout<<ans-n<<endl;
+    cout<<ans<<endl;
 }
 
 signed main(){
     cin.tie(0)->sync_with_stdio(0);
-    int t=1; while(t--)code();
+    int t; cin>>t; while(t--)code();
     return 0;
-}  
+}
