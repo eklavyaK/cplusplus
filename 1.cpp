f0c7cf4434ef02be05671fb07ec2adf13a95e3ba
eklavya <eklavyakumarsingh100@gmail.com>
2023-03-22 23:45:34 +0530

    commit

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,4 @@
+
 #include<bits/stdc++.h>
 #define endl "\n"
 #define F first
@@ -10,8 +11,44 @@ using namespace std;
 
 
 void code(){
-    
-
+    int n,m,k; cin>>n>>m>>k;
+    int curr = -2, l = 1;
+    map<int,int> uu, vv;
+    for(int i=0;i<n;i++){
+        int r; cin>>r;
+        if(curr==r-1) uu[r]=uu[r-1];
+        else uu[r]=l++;
+        curr = r;
+    }
+    uu[1e6+1] = -1;
+    l = 1, curr = -2;
+    for(int i=0;i<m;i++){
+        int r; cin>>r;
+        if(curr==r-1) vv[r]=vv[r-1];
+        else vv[r] = l++;
+        curr = r;
+    }
+    vv[1e6+1] = -1;
+    map<int,pair<map<int,int>,int>> uuuu,vvvv;
+    for(int i=0;i<k;i++){
+        int u,v; cin>>u>>v;
+        if(!uu.count(u)){
+            int s = uu.lower_bound(u)->F;
+            uuuu[s].F[vv[v]]++; uuuu[s].S++;
+        }
+        else if(!vv.count(v)){
+            int s = vv.lower_bound(v)->F;
+            vvvv[s].F[uu[u]]++; vvvv[s].S++;
+        }
+    }
+    int ans = 0;
+    for(auto i : uuuu){
+        for(auto j : i.S.F) ans += (i.S.S-j.S)*j.S;
+    }
+    for(auto i : vvvv){
+        for(auto j : i.S.F) ans += (i.S.S-j.S)*j.S;
+    }
+    cout<<(ans>>1)<<endl;
 }
 
 signed main(){
