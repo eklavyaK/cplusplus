b4bf8e09e123071268d23f3bbcc83b5f7b6e6fd1
eklavya <eklavyakumarsingh100@gmail.com>
2023-03-25 22:23:12 +0530

    commit

--- 1.cpp
+++ 1.cpp
@@ -10,8 +10,17 @@ using namespace std;
 
 
 void code(){
-    
-
+    int n,k,d,w; cin>>n>>k>>d>>w;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        ans++;
+        int c = k-1;
+        int open = arr[i]+w;
+        while(i+1<n && arr[i+1]<=open+d && c>0)i++,c--;
+    }
+    cout<<ans<<endl;
 }
 
 signed main(){
