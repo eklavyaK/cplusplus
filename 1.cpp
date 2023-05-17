fc138d4da987edcb1e11b96deebe32aa3568db6d
eklavya <eklavyakumarsingh100@gmail.com>
2023-05-17 17:30:19 +0530

    commit

--- 1.cpp
+++ 1.cpp
@@ -10,8 +10,24 @@ using namespace std;
 
 
 void code(){
-    
-
+    int n; cin>>n;
+    int arr[n],f[n]{1};
+    for(int i=0;i<n;i++)cin>>arr[i];
+    int x; cin>>x;
+    if(n>1 && arr[0]+arr[1]>=x*2) f[1] = 1;
+    for(int i=2;i<n;i++){
+        if(f[i-1] && f[i-2]){
+            if(arr[i-1]+arr[i-2]+arr[i]>=x*3) f[i] = 1;
+            if(arr[i-1]+arr[i]<x*2) f[i] = 0;
+        }
+        else if(f[i-1]){
+            if(arr[i-1]+arr[i]>=x*2) f[i] = 1;
+        }
+        else f[i] = 1;
+    }
+    int ans = 0;
+    for(auto i : f) ans+=i;
+    cout<<ans<<endl;
 }
 
 signed main(){
