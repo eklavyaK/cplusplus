86d79cee398922f04ab17b81acae36b017ace25d
eklavya <eklavyakumarsingh100@gmail.com>
2023-05-18 16:06:31 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -12,45 +12,12 @@ using namespace std;
 void code(){
     int n; cin>>n;
     int arr[n];
-    for(int i=0;i<n;i++)cin>>arr[i];
-    int x, ans = 0;; cin>>x;
-    int done[n]{};
     for(int i=0;i<n;i++){
-        if(arr[i]<x) continue;
-        int l = i, sum = arr[i];
-        done[i] = 1;
-        while(i+1<n && arr[i+1]>=x){
-            i++; sum+=arr[i]; done[i] = 1;
-        }
-        if(i==l){
-            done[i] = 0; continue;
-        }
-        if(l-1>=0 && done[l-1]!=-1){
-            if(arr[l-1]+arr[l]<x*2) done[l-1] = -1;
-            else if(arr[l-1]+arr[l]>=x*2) done[l-1] = 1;
-            if(done[l-1]==1) sum+=arr[l-1], --l;
-        }
-        if(i+1<n){
-            if(arr[i+1]+arr[i]>=2*x && sum+arr[i+1]>=x*(i+2-l)) done[i+1] = 1;
-            else done[i+1]=-1;
-        }
+        int k; cin>>k;
+        cout<<(!k)<<' ';
+
     }
-    for(int i=0;i<n;i++){
-        if(arr[i]>=x && !done[i]){
-            if(i-1>=0 && i+1<n && done[i+1]==1 && done[i-1]==1){
-                if(arr[i]+arr[i+1]+arr[i-1]>=x*3) done[i] = 1;
-            }
-            else{
-                done[i] = 1;
-                if(i-1>=0 && done[i-1]!=-1 && arr[i]+arr[i-1]>=x*2) done[i-1] = 1;
-                else if(i-1>=0) done[i-1] = -1;
-                if(i+1<n && done[i+1]!=-1 && arr[i]+arr[i-1]*(done[i-1]==1)+arr[i+1]>=x*(2+(done[i-1]==1))) done[i+1] = 1; 
-                else if(i+1<n) done[i+1] = -1;
-            }
-        }
-    }
-    for(auto i : done) ans+=(i==1);
-    cout<<ans<<endl;
+    cout<<endl;
 }
 
 signed main(){
