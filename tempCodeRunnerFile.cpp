fbafe7aa7d0d8c623be8f4070319335fd747760c
eklavya <eklavyakumarsingh100@gmail.com>
2023-05-23 18:44:09 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,27 +1,24 @@
-#include<bits/stdc++.h>
-#define endl "\n"
-#define F first
-#define S second
-#define int long long
-typedef long long ll;
-typedef long double ld;
-using namespace std;
-
-
-
-void code(){
-    int n; cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        int k; cin>>k;
-        cout<<(!k)<<' ';
-
-    }
-    cout<<endl;
-}
-
-signed main(){
-    cin.tie(0)->sync_with_stdio(0);
-    int t; cin>>t; while(t--)code();
-    return 0;
-}
+function<void(int,int)> dfs = [&](int i, int j){
+    //     if(i<0) return;
+    //     if(s[i]=='(' && s[j]==')'){
+    //         cost+=active;
+    //         if(!active){
+    //             i-=2;
+    //             address[i+1] = j;
+    //             j = address[i];
+    //         }
+    //         else{
+    //             i--;
+    //             j = address[j];
+    //             active--;
+    //         }
+    //     }
+    //     else if(s[i]==')' && s[j]==')'){
+    //         address[i] = j;
+    //         j = i;
+    //         i--;
+    //         active++;
+    //     }
+    //     dfs(i,j);
+    // };
+    // dfs(n-2,n-1);
