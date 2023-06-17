013aa60014ee5fbef2f7287dd4c563690d663d43
eklavya <eklavyakumarsingh100@gmail.com>
2023-06-17 23:05:34 +0530

    commit

--- ConstructOR.cpp
+++ ConstructOR.cpp
@@ -14,18 +14,28 @@ using namespace std;
 #define debugvar(c)
 #endif
 
-
-
-
-
 void code(int TC){
-    
-
+    int a,b,d; cin>>a>>b>>d;
+    int x = a|b, lsx = -1, lsd = -1;
+    for(int i=0;i<31;i++){
+        if(lsx==-1 && ((1<<i)&x)) lsx = i;
+        if(lsd==-1 && ((1<<i)&d)) lsd = i;
+    }
+    if(lsd>lsx){
+        cout<<-1<<endl;
+        return;
+    }
+    int ans = d;
+    for(int i=0;i<30;i++){
+        if(((1<<i)&x) && !((1<<i)&ans)) ans += (d<<(i-lsd));
+    }
+    cout<<ans<<endl;
 }
 
 
 signed main(){
     cin.tie(0)->sync_with_stdio(0);
+    cerr.tie(0)->sync_with_stdio(0);
     int TT = 1; cin >> TT;
     for (int TC = 1; TC <= TT; TC++) 
         code(TC);
