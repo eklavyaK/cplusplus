0fa3dd94455aff71a2dddf58c606d3682d2089f6
eklavya <eklavyakumarsingh100@gmail.com>
2023-06-23 18:44:28 +0530

    commit

--- 2.cpp
+++ 2.cpp
@@ -1,7 +1,11 @@
 #include<bits/stdc++.h>
-#define pii pair<int,int>
+#define endl "\n"
+#define F first
+#define S second
+#define int long long
+typedef long long ll;
+typedef long double ld;
 using namespace std;
-const int N=1e5+5;
 #ifndef ONLINE_JUDGE
 #include "include/debug.h"
 #else
@@ -9,61 +13,28 @@ const int N=1e5+5;
 #define debugarr(a,n)
 #define debugvar(c)
 #endif
-int l[N],r[N];
-int ans[30];
 
-vector<pii> f;
 
-bool cmp(pii a,pii b){
-	if(a.first==b.first){
-		return a.second < b.second;
-	}
-	return a.first < b.first;
+
+
+
+void code(int TC){
+    priority_queue<int> q;
+    q.push(1);
+    q.push(1);
+    q.push(1);
+    while(!q.empty()){
+        int k = q.top(); q.pop();
+        cout<<k<<endl;
+    }
 }
 
-int main(){
-	int n;
-	cin>>n;
-	for(int i=1;i<=n;i++){
-		scanf("%d%d",&l[i],&r[i]);
-	}
-	for(int i=0;i<=29;i++){
-		int step=(1<<i);
-		f.clear();
-		for(int j=1;j<=n;j++){
-			if(r[j]-l[j]+1>=step){
-				f.push_back({0,1});
-				f.push_back({step,-1});
-				continue;
-			}
-			int x=l[j]%step;
-			int y=x+r[j]-l[j];
-			f.push_back({0,1});
-			f.push_back({max(y-step+1,0),-1});
-			f.push_back({x,1});
-			f.push_back({min(step,y+1),-1});
-		}
-		sort(f.begin(),f.end(),cmp);
-		int num=0;
-		for(int j=0;j<f.size();j++){
-			num+=f[j].second;
-			ans[i]=max(ans[i],num); 
-		}
-	}
-    debugarr(ans,30);
-	// int q;
-	// cin>>q;
-	// while(q--){
-	// 	int w;
-	// 	scanf("%d",&w);
-	// 	int k;
-	// 	for(int i=0;i<=29;i++){
-	// 		if((w>>i)&1){
-	// 			k=i;
-	// 			break;
-	// 		}
-	// 	}
-	// 	printf("%d\n",ans[k]);
-	// }
-	// return 0;
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);cerr.tie(0);
+    int TT = 1; cin >> TT;
+    for (int TC = 1; TC <= TT; TC++) 
+        code(TC);
+    return 0;
 }
--- A_Prefix_and_Suffix_Array.cpp
+++ A_Prefix_and_Suffix_Array.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define endl "\n"
 #define F first
 #define S second
@@ -7,29 +7,41 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
-
-
-void code(){
-    int n; cin>>n;
+void code()
+{
+    int n;
+    cin >> n;
     vector<string> st;
     vector<char> ch;
-    for(int i=0;i<2*n-2;i++){
-        string s; cin>>s;
-        if(s.size()==n-1)st.push_back(s);
-        if(s.size()==1) ch.push_back(s[0]);
+    for (int i = 0; i < 2 * n - 2; i++)
+    {
+        string s;
+        cin >> s;
+        if (s.size() == n - 1)
+            st.push_back(s);
+        if (s.size() == 1)
+            ch.push_back(s[0]);
     }
-    if(ch[0]==ch[1]){
-        reverse(st[0].begin(),st[0].end());
-        if(st[0]==st[1]){
-            cout<<"YES"<<endl;
+    if (ch[0] == ch[1])
+    {
+        reverse(st[0].begin(), st[0].end());
+        if (st[0] == st[1])
+        {
+            cout << "YES" << endl;
         }
-        else cout<<"NO"<<endl;
+        else
+            cout << "NO" << endl;
     }
-    else cout<<"NO"<<endl;
+    else
+        cout << "NO" << endl;
 }
 
-signed main(){
+signed main()
+{
     cin.tie(0)->sync_with_stdio(0);
-    int t; cin>>t; while(t--)code();
+    int t;
+    cin >> t;
+    while (t--)
+        code();
     return 0;
 }
