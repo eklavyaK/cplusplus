57a4bd74fa335fda8b2e2a3db9ab1b3d687dbd8b
eklavya <eklavyakumarsingh100@gmail.com>
2023-07-15 18:20:23 +0530

    commit

--- 1.cpp
+++ 1.cpp
@@ -14,19 +14,43 @@ using namespace std;
 #endif
 
 
-
+struct Node{
+    int h, c;
+};
 
 
 void code(int TC){
-    
-
+    int n,h; cin>>n>>h;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    vector<vector<Node>> dp(n,vector<Node>(n,Node{-1,-1}));
+    for(int i=0;i<n;i++) dp[i][i] = {h,0};
+    function<Node(int,int)>  join = [&](int l, int r){
+        int mid = (arr[l]+arr[r])>>1;
+        if(arr[r]-mid>h) return Node{0, 2*h};
+        else return Node{h-(arr[r]-mid),2*(arr[r]-mid)-1};
+    };
+    function<Node(int,int)> calc = [&](int l, int r){
+        if(r<l) return Node{-1,-1};
+        if(dp[l][r].h!=-1) return dp[l][r];
+        auto [nh,e] = join(l,r);
+        dp[l][r].h = nh, dp[l][r].c = 1e18;
+        for(int i=l;i<r;i++){
+            if(dp[l][i].h==-1) dp[l][i] = calc(l,i);
+            if(dp[i+1][r].h==-1) dp[i+1][r] = calc(i+1,r);
+            dp[l][r].c = min(dp[l][r].c, dp[l][i].c+dp[i+1][r].c+e+dp[l][i].h+dp[i+1][r].h-2*h);
+        }
+        return dp[l][r];
+    };
+    calc(0,n-1);
+    cout<<dp[0][n-1].h+dp[0][n-1].c<<endl;
 }
 
 
 signed main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);cerr.tie(0);
-    int TT = 1; cin >> TT;
+    int TT = 1;
     for (int TC = 1; TC <= TT; TC++) 
         code(TC);
     return 0;
--- 2.cpp
+++ 2.cpp
@@ -12,79 +12,16 @@ using namespace std;
 #define debugarr(a,n) 42
 #define debug(...) 42
 #endif
-bool check(vector<vector<pair<int,int>>> org, int cap, int n){
-    debug(cap);
-    vector<vector<int>> graph(n+1),rev(n+1);
-    vector<int> id(n+1), sz(n+1,1), idg(n+1);
-    iota(id.begin(),id.end(),0);
-    function<int(int)> find_par = [&](int u){
-        if(u==id[u]) return u;
-        return id[u] = find_par(id[u]);
-    };
-    auto merge = [&](int u, int v){
-        u = find_par(u), v = find_par(v);
-        if(u!=v){
-            if(sz[u]<sz[v]) swap(u,v);
-            sz[u] += sz[v], id[v] = u;
-        }
-    };
-    for(int i=1;i<=n;i++){
-        for(auto [u,v] : org[i]){
-            if(v<=cap) merge(i,u);
-        }
-    }
-    for(int i=1;i<=n;i++) id[i] = find_par(i);
-    for(int i=1;i<=n;i++){
-        for(auto [u,v] : org[i]){
-            if(v>cap && id[u]!=id[i]) graph[id[i]].push_back(id[u]),rev[id[u]].push_back(id[i]), idg[id[u]] = 1;
-        }
-    }
-    debug(id);
-    debug(graph);
-    fill(sz.begin(),sz.end(),0);
-    vector<int> all;
-    function<void(int)> find_mother = [&](int node){
-        sz[node] = 1;
-        for(auto u : rev[node]){
-            if(!sz[u]) find_mother(u);
-        }
-        all.push_back(node);
-    };
-    find_mother(id[1]);
-    int mother = 0;
-    for(auto i : all){
-        mother = i;
-        if(!idg[i]) break;
-    }
-    debug(mother);
-    fill(sz.begin(),sz.end(),0);
-    function<void(int)> dfs = [&](int node){
-        sz[node] = 1;
-        for(auto u : graph[node]){
-            if(!sz[u]) dfs(u);
-        }
-    };
-    dfs(mother);
-    for(int i=1;i<=n;i++) if(!sz[id[i]]) return false;
-    return true;
-}
+
+
+struct Node{
+    int l;
+};
 
 
 void code(int TC){
-    int n,m; cin>>n>>m;
-    vector<vector<pair<int,int>>> graph(n+1);
-    int l = 0, r = 0, ans = -1;
-    while(m--){
-        int u,v,w; cin>>u>>v>>w;
-        graph[u].push_back({v,w});
-        r = max(r,w);
-    }
-    while(l<=r){
-        int mid = (l+r)>>1;
-        if(check(graph,mid,n)) r = mid-1, ans = mid;
-        else l = mid+1;
-    }
-    cout<<ans<<endl;
+    
+    vector<Node> v(3);
 }
 
 
