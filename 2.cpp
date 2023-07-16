4e55f6d037938853565d17529fb13fc8707669c5
eklavya <eklavyakumarsingh100@gmail.com>
2023-07-16 16:51:38 +0530

    commit

--- 2.cpp
+++ 2.cpp
@@ -1,97 +1,59 @@
 #include<bits/stdc++.h>
-/*#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds; 
-#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> 
-*/
-using namespace std;
-#define fastio  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define ll long long
-#define vi vector<int>
-#define si set<int>
-#define mii map<int,int>
-#define pb push_back
-#define PI acos(-1)
-#define pii pair<int,int>
 #define endl "\n"
-#define all(a) a.begin(),a.end()
-#define len(s) (int)s.size()
 #define F first
 #define S second
-const int mod=1e9+7;
-int dp[20001][101][101];
-int a[101];
-const int add=10000;
-const int L=-10000;
-const int R=10000;
-int f(int sum,int l,int r)
-{
-	//count number of non-empty subsequence of subarray [l,r] 
-	//with adding=sum
-	if( !(sum>=L and sum<=R))
-		return 0;
-	
-	if(l==r)
-	{
+#define int long long
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "include/debug.h"
+#else
+#define debugarr(a,n) 42
+#define debug(...) 42
+#endif
 
-		if(sum==a[l])
-			return 1;
-		return 0;
-	}
-	if(dp[sum+add][l][r]!=-1)
-		return dp[sum+add][l][r];
-	int ans1=f(sum,l,r-1);
-	int ans2=f(sum,l+1,r);
-	int rem=0;
-	if(l+1<=r-1)
-		rem=f(sum,l+1,r-1);
-	int ans3=0;
-	if(l+1<=r-1)
-		ans3=f(sum-a[l]-a[r],l+1,r-1)+(sum==(a[l]+a[r]));
-	else
-		ans3=(sum==(a[l]+a[r]));
-	
-	return dp[sum+add][l][r]=(((ans1+ans2)%mod-rem+mod)%mod+ans3)%mod;
-}
-void solve()
-{
-	int n,x;
-	cin>>n>>x;
-	memset(dp,-1,sizeof(dp));
-	for(int i=1;i<=n;i++)
-		cin>>a[i];
-	if(n<3)
-	{
-		cout<<0;
-		return ;
-	}
-	else
-	{
-		sort(a+1,a+n+1);
-		for(int i=1;i<=n;i++)
-			a[i]-=x;
-		int ans=0;
-	
-		for(int i=1;i<=n;i++)
-		{
-			for(int j=i+2;j<=n;j++)
-			{
-				ans+=f(0,i+1,j-1);
-				ans%=mod;
-                if(i==1)cout<<ans<<endl;
-			}
+const int N = 1e3;
+int par[N], node[N][3], J[N][N], n, k, P = 0;
+void calc(){
+	for(int i=0;i<k;i++){
+		vector<int> R;
+		for(int j=0;j<n;j++) if(!par[J[i][j]]) R.push_back(J[i][j]);
+		int sz = R.size();
+		for(int j=0;j<sz-2;j++){
+			if(node[R[j+1]][0]==min(R[j],R[j+2]) && node[R[j+1]][1]==max(R[j],R[j+2])) node[R[j+1]][2]++;
+			else node[R[j+1]][0] = min(R[j],R[j+2]), node[R[j+1]][1] = max(R[j],R[j+2]), node[R[j+1]][2] = 1;
 		}
-		cout<<ans;
 	}
+	for(int i=1;i<=n;i++) if(node[i][2]==k) par[node[i][0]] = par[node[i][1]] = i, P += 2;
 }
-signed main()
-{
-fastio
-int TestCases=1;
-//cin>>TestCases;
-while(TestCases--)
-{
-solve();
-cout<<endl;
+void code(int TC){
+	cin>>n>>k;
+	for(int i=0;i<k;i++){
+		for(int j=0;j<n;j++) cin>>J[i][j];
+	}
+	for(int i=0;i<(n+1)/2;i++) calc();
+	queue<int> q;
+	for(int i=1;i<=n;i++){
+		if(!par[i]) q.push(i);
+	}
+	while(q.size()>2){
+		int a = q.front(); q.pop();
+		int b = q.front(); q.pop();
+		int c = q.front(); q.pop();
+		par[a] = par[b] = c;
+		q.push(c);
+	}
+	while(!q.empty()) par[q.front()] = -1, q.pop();
+	for(int i=1;i<=n;i++) cout<<par[i]<<" ";cout<<endl;
 }
+
+
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);cerr.tie(0);
+	int TT = 1;
+	for (int TC = 1; TC <= TT; TC++) 
+		code(TC);
+	return 0;
 }
