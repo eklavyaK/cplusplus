a34d550d36cd0d932df1ea6b17f82346308a0225
eklavya <eklavyakumarsingh100@gmail.com>
2023-08-31 01:06:36 +0530

    commit

--- ContestGenerators/Practice.py
+++ ContestGenerators/Practice.py
@@ -4,9 +4,9 @@ import random
 
 PA = random.randint(16, 17) * 100
 PB = random.randint(18, 19) * 100
-PC = random.randint(20, 20) * 100
-PD = random.randint(21, 21) * 100
-PE = random.randint(22, 22) * 100
+PC = random.randint(20, 21) * 100
+PD = random.randint(22, 22) * 100
+PE = random.randint(23, 23) * 100
 PF = random.randint(23, 23) * 100
 PG = random.randint(23, 23) * 100
 
@@ -18,19 +18,22 @@ print("E: ", PE)
 print("F: ", PF)
 print("G: ", PG)
 
+"math, graphs, trees, dp, binary%20search, probabilities, number%20theory, divide%20and%20conquer, strings"
+
+tags = "math"
 
 webbrowser.open(f'https://codeforces.com/mashup/new', new = 2)
 time.sleep(1)
-webbrowser.open(f'https://codeforces.com/problemset?tags={PA}-{PA}', new = 2)
+webbrowser.open(f'https://codeforces.com/problemset?tags={tags},{PA}-{PA}', new = 2)
 time.sleep(1)
-webbrowser.open(f'https://codeforces.com/problemset?tags={PB}-{PB}', new = 2)
+webbrowser.open(f'https://codeforces.com/problemset?tags={tags},{PB}-{PB}', new = 2)
 time.sleep(1)
-webbrowser.open(f'https://codeforces.com/problemset?tags={PC}-{PC}', new = 2)
+webbrowser.open(f'https://codeforces.com/problemset?tags={tags},{PC}-{PC}', new = 2)
 time.sleep(1)
-webbrowser.open(f'https://codeforces.com/problemset?tags={PD}-{PD}', new = 2)
-time.sleep(1)
-webbrowser.open(f'https://codeforces.com/problemset?tags={PE}-{PE}', new = 2)
-time.sleep(1)
-webbrowser.open(f'https://codeforces.com/problemset?tags={PF}-{PF}', new = 2)
-time.sleep(1)
-webbrowser.open(f'https://codeforces.com/problemset?tags={PG}-{PG}', new = 2)
+webbrowser.open(f'https://codeforces.com/problemset?tags={tags},{PD}-{PD}', new = 2)
+# time.sleep(1)
+# webbrowser.open(f'https://codeforces.com/problemset?tags={tags},{PE}-{PE}', new = 2)
+# time.sleep(1)
+# webbrowser.open(f'https://codeforces.com/problemset?tags={tags},{PF}-{PF}', new = 2)
+# time.sleep(1)
+# webbrowser.open(f'https://codeforces.com/problemset?tags={tags},{PG}-{PG}', new = 2)
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -14,66 +14,48 @@ using namespace std;
 #endif
 
 
-const int N = 2005;
 
-vector<vector<int>> T(N);
-vector<int> mark(N), I(N);
 
-int qry(int u, int v){
-	cout << "? " << u << " " << v << endl << flush;
-	cin >> u; return u;
-}
-
-void remove(int u, int p){
-	mark[u] = 1;
-	for(auto v : T[u]) if(v != p && !mark[v]) remove(v, u);
-}
 
 void code(int TC){
-	int n; cin >> n;
-	int mxI = 0, uI = 0, edges = 0, rt = 1;
-	for(int i = 0; i < n - 1; i++){
+	int n, m, k; cin >> n >> m >> k;
+	vector<vector<int>> G(n + 5);
+	vector<pair<int,int>> ranges, T(n + 5);
+	vector<int> h(n + 5), I(n + 5), O(n + 5);
+	for(int i = 1; i <= n; i++) cin >> h[i], T[i] = {0, h[i]};
+	vector<int> p(n + 5);
+	iota(p.begin(), p.end(), 0);
+	for(int j = 0; j < m; j++){
 		int u, v; cin >> u >> v;
-		T[u].push_back(v);
-		T[v].push_back(u);
-		I[u] += 1, I[v] += 1;
-		if(I[u] > mxI) mxI = I[u], uI = u;
-		if(I[v] > mxI) mxI = I[v], uI = v;
-		edges = edges + 1;
+		G[u].push_back(v);
+		p[v] = u;
+		I[v] += 1, O[u] += 1;
 	}
-	while(true){
-		if(edges == 0){
-			cout << "! " << rt << endl << flush;
-			return;
-		}
-		else if(edges == 1){
-			for(auto u : T[rt]){
-				rt = qry(rt, u);
-			}
-			edges = 0;
-		}
-		else{
-			rt = qry(T[uI][0], T[uI][1]);
-			if(rt != uI) remove(uI, rt);
-			else remove(T[uI][0], rt), remove(T[uI][1], rt);
-			I.assign(N, 0);
-			mxI = 0, uI = 0, edges = 0;
-			vector<vector<int>> newT(N);
-			for(int i = 1; i <= n; i++){
-				for(int j = 0; j < T[i].size(); j++){
-					if(mark[T[i][j]] || T[i][j] < i) continue;
-					debug(T[i][j], i);
-					newT[T[i][j]].push_back(i);
-					newT[i].push_back(T[i][j]);
-					I[i] += 1, I[T[i][j]] += 1;
-					if(I[i] > mxI) mxI = I[i], uI = i;
-					if(I[T[i][j]] > mxI) mxI = I[T[i][j]], uI = T[i][j];
-					edges = edges + 1;
-				}
-			}
-			T = newT;
+	queue<int> q;
+	for(int i = 1; i <= n; i++) if(I[i] == 0) q.push(i);
+	while(!q.empty()){
+		int u = q.front(); q.pop();
+		for(auto v : G[u]){
+			if(T[u].ss > T[v].ss) T[v].ff = max(T[u].ff + 1, T[v].ff), p[v] = p[u];
+			else if(T[u].ff > T[v].ff) T[v].ff = T[u].ff, p[v] = p[u];
+			I[v]--;
+			if(I[v] == 0) q.push(v);
 		}
 	}
+	debug(p);
+	for(int i = 1; i <= n; i++) if(O[i] == 0) ranges.push_back({T[p[i]].ff * k + T[p[i]].ss, T[i].ff * k + T[i].ss});
+	sort(ranges.begin(), ranges.end());
+	debug(ranges);
+	n = ranges.size();
+	int mx = -1e18, ans = 1e18;
+	vector<int> M(n);
+	for(int i = n - 1; i >= 0; i--) mx = max(mx, ranges[i].ss), M[i] = mx;
+	mx = -1e18;
+	for(int i = 0; i < n; i++){ 
+		ans = min(ans, max(M[i], mx + k) - ranges[i].ff);
+		mx = max(ranges[i].ss, mx);
+	}
+	cout << ans << endl;
 }
 
 
@@ -81,7 +63,7 @@ signed main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);cerr.tie(0);
 	cout.precision(30);
-	int TT = 1;
+	int TT = 1; cin >> TT;
 	for (int TC = 1; TC <= TT; TC++) 
 		code(TC);
 	return 0;
