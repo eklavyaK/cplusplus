286e3fa704f2e39160ebd6ce57853fdac1d15e98
eklavya <eklavyakumarsingh100@gmail.com>
2023-09-06 17:18:24 +0530

    commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+#define ff first
+#define ss second
+#define int long long
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "include/debug.h"
+#else
+#define debugarr(a, n) 42
+#define debug(...) 42
+#endif
+
+
+
+
+
+void code(int TC){
+	int n; cin >> n;
+	vector<int> a(n);
+	int ans = 1e9;
+	for(int i = 0; i < n; i++) cin >> a[i], ans = min(ans, abs(a[i]));
+	cout << ans << endl;
+
+}
+
+
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);cerr.tie(0);
+	cout.precision(30);
+	int TT = 1; cin >> TT;
+	for (int TC = 1; TC <= TT; TC++) 
+		code(TC);
+	return 0;
+}
