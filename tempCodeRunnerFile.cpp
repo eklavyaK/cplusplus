702ad0f00c1d28846d2bac8f115a88a25fcd10ab
eklavya <eklavyakumarsingh100@gmail.com>
2023-10-08 03:34:27 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,37 +1,7 @@
-#include<bits/stdc++.h>
-#define endl "\n"
-#define ff first
-#define ss second
-#define int long long
-typedef long long ll;
-typedef long double ld;
-using namespace std;
-#ifndef ONLINE_JUDGE
-#include "include/debug.h"
-#else
-#define debugarr(a, n) 42
-#define debug(...) 42
-#endif
-
-
-
-
-
-void code(int TC){
-	int n, k, x; cin >> n >> k >> x;
-	if(n * k - (k - 1) * k / 2 >= x){
-		cout << "YES" << endl;
-	}
-	else cout << "NO" << endl;
-}
-
-
-signed main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);cout.tie(0);cerr.tie(0);
-	cout.precision(30);
-	int TT = 1; cin >> TT;
-	for (int TC = 1; TC <= TT; TC++) 
-		code(TC);
-	return 0;
-}
+			long long L = p[l] - 1;
+			L = min(L, (long long)r);
+			string high = to_string(L);
+			int sz = high.size();
+			for(int i = 0; i < (1 << sz); i += 1){
+			if(2 * __builtin_popcount(i) != sz) continue;
+			vector<vector<long long>> dp(2, vector<long long> (k));
