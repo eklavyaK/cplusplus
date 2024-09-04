e6c92af2de1042abc859f60bda363290b2a17227
eklavya <eklavyakumarsingh100@gmail.com>
2024-09-04 23:57:16 +0530

    commit

--- 1.cpp
+++ 1.cpp
@@ -1,39 +1,34 @@
 #include<bits/stdc++.h>
+#define endl "\n"
+#define ff first
+#define ss second
+#define int long long
+typedef long long ll;
+typedef long double ld;
 using namespace std;
+#ifndef ONLINE_JUDGE
+#include "include/debug.h"
+#else
+#define debugarr(a, n) 42
+#define debug(...) 42
+#endif
 
 
+
+
+
+void code(int TC){
+	
+
+}
 
 
 signed main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);cerr.tie(0);
-	int tt; cin >> tt;
-	while(tt--){
-		int n; cin >> n;
-		vector<int> a(n);
-		for(int i = 0; i < n; i++) cin >> a[i];
-		int l = 0, ans = 0;
-		for(int i = 0; i < n; i++){
-			if(a[i] == 0){
-				l = 0;
-				continue;
-			}
-			if(l == 1 && a[i] <= 2){
-				l = 0;
-				continue;
-			}
-			if(a[i] <= 2){
-				ans += 1;
-				l = 1;
-			}
-			else{
-				if(l == 1 && i != n - 1 && a[i] <= 4 && a[i + 1] <= 4){
-					a[i + 1] = min(a[i + 1], 2);
-				}
-				ans += 1;
-				l = 0;
-			}
-		}
-		cout <<  ans << endl;
-	}
+	cout.precision(30);
+	int TT = 1; cin >> TT;
+	for (int TC = 1; TC <= TT; TC++) 
+		code(TC);
+	return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -19,15 +19,39 @@ using namespace std;
 
 void code(int TC){
 	int n, k; cin >> n >> k;
-	vector<int> a(n);
-	for(int i = 0; i < n; i++) cin >> a[i];
-	sort(a.begin(), a.end());
-	k = n - k;
-	int ans = a[n - 1] - a[0];
-	for(int i = k - 1; i < n; i++){
-		ans = min(ans, a[i] - a[i - k + 1]);
+	map<int, int> m;
+	for(int i = 0; i < n; i++){
+		int x; cin >> x;
+		m[x] += 1;
 	}
-	cout << ans << endl;
+	vector<int> vec;
+	for(auto [u, v] : m){
+		vec.push_back(u);
+	}
+	int g = vec[0];
+	for(int i = 1; i < vec.size(); i++){
+		g = gcd(g, vec[i] - vec[i - 1]);
+	}
+	set<int> st;
+	if(n == 1){
+		if(k >= vec[0] + 1) cout << k << endl;
+		else cout << k - 1 << endl;
+	}
+	else{
+		if(k > (n - 1) * g - (n - 1)){
+			cout << k + (n - 1) << endl;
+			return;
+		}
+		else{
+			int l = 1, r = (n - 1) * g - 1, ans = 1;
+			while(l <= r){
+				int mid = (l + r) >> 1;
+				if(k >= mid - mid / g) l = mid + 1, ans = mid;
+				else r = mid - 1;
+			}
+			cout << ans << endl;
+		}
+	}
 }
 
 
@@ -35,7 +59,7 @@ signed main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);cerr.tie(0);
 	cout.precision(30);
-	int TT = 1;
+	int TT = 1; cin >> TT;
 	for (int TC = 1; TC <= TT; TC++) 
 		code(TC);
 	return 0;
